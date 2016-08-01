5ffd4a5cb9137f44959e2462b2fac576927909a0
Anil Kumar Beeram <anilkumarb404@gmail.com>
2016-08-01 23:21:14 +0530

    using undersore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var PORT = process.env.PORT || 3000;
 var todos = [{
 	description: 'Going grocery shopping',
@@ -24,7 +25,11 @@ app.get('/',function(req, res, next){
 });
 
 app.post('/todos',function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+	console.log(body);
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
 	var currId = todos.length;
 	currId++;
 	body.id = currId;
@@ -37,13 +42,8 @@ app.get('/todos',function(req, res, next){
 });
 
 app.get('/todos/:id',function(req, res, next){
-	var todoId = req.params.id;
-	var matchedTodo;
-	todos.forEach(function(item){
-		if(item.id == todoId){
-			matchedTodo = item;
-		}
-	});
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos,{id:todoId});
 	
 	if(matchedTodo){
 		res.json(matchedTodo);
