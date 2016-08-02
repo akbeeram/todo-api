528ac2cb84367e5c3c8770dd7fdbb1f66119c392
Anil Kumar Beeram <anilkumarb404@gmail.com>
2016-08-02 09:13:27 +0530

    Adding PUT API for updating todos

--- server.js
+++ server.js
@@ -35,6 +35,30 @@ app.delete('/todos/:id',function(req, res, next){
 	}
 });
 
+app.put('/todos/:id',function(req, res){
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos,{id:todoId});
+	var body = _.pick(req.body,'description','completed');
+	var validAttributes = {};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	}
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(!_.isString(body.description)){
+		return res.status(400).send();	
+	}
+	
+	_.extend(matchedTodo,validAttributes);
+	res.json(todos);
+});
+
 app.post('/todos',function(req, res){
 	var body = _.pick(req.body,'description','completed');
 	console.log(body);
