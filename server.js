5fc1bed6f24c0404af6229cd59a00bb4fcc02e7a
Anil Kumar Beeram <anilkumarb404@gmail.com>
2016-08-02 10:05:30 +0530

    with two query params

--- server.js
+++ server.js
@@ -80,6 +80,11 @@ app.get('/todos',function(req, res, next){
 	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos = _.where(todos,{completed:false});
 	}
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos,function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
 	res.json(filteredTodos);
 });
 
