9a18a715f610dfd58fdf33e827ca4a9a83d818a1
Anil Kumar Beeram <anilkumarb404@gmail.com>
2016-09-15 15:31:11 +0530

    convert /delete to sequelize

--- server.js
+++ server.js
@@ -27,13 +27,29 @@ app.get('/',function(req, res, next){
 
 app.delete('/todos/:id',function(req, res, next){
 	var todoId = parseInt(req.params.id);
-	var matchedTodo = _.findWhere(todos,{id:todoId});
-	if(!matchedTodo){
-		res.status(400).json({"error":"no todo found with that id"});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(todos);
-	}
+
+	db.todo.destroy({
+		where:{
+			id:todoId
+		}
+	}).then(function(rowsDeleted){
+		if(rowsDeleted === 0){
+			res.status(404).json({
+				error:'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	},function(){
+		res.status(500).send();
+	});
+// 	var matchedTodo = _.findWhere(todos,{id:todoId});
+// 	if(!matchedTodo){
+// 		res.status(400).json({"error":"no todo found with that id"});
+// 	} else {
+// 		todos = _.without(todos, matchedTodo);
+// 		res.json(todos);
+// 	}
 });
 
 app.put('/todos/:id',function(req, res){
